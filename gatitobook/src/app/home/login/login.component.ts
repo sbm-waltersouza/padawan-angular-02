1a9111311b22f6308092c38ce803a8e87989727f
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-06-07 13:21:12 -0300

    Consumindo o serviço de autenticação

--- gatitobook/src/app/home/login/login.component.ts
+++ gatitobook/src/app/home/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AutenticacaoService } from 'src/app/autenticacao/autenticacao.service';
 
 @Component({
   selector: 'app-login',
@@ -7,17 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  usuario = '';
-  senha = '';
+  usuario='';
+  senha='';
 
-  constructor() { }
+  constructor(private authService: AutenticacaoService, private router: Router) { }
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {  }
 
   login() {
-    console.log(this.usuario)
-    console.log(this.senha)
+    this.authService.autenticar(this.usuario, this.senha).subscribe(() => {
+      this.router.navigate(['animais']);
+    },(error) => {
+      alert('Usuário ou senha inválido');
+      console.log(error);
+    });
   }
 }
