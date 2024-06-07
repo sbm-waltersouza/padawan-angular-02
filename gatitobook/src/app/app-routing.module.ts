72e2039940ab23fea3ec17dbf13ab7554265e625
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-06-07 14:02:49 -0300

    Redirecionando a rota

--- gatitobook/src/app/app-routing.module.ts
+++ gatitobook/src/app/app-routing.module.ts
@@ -1,4 +1,3 @@
-
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -11,6 +10,10 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'animais',
+    loadChildren: () => import('./animais/animais.module').then((m) => m.AnimaisModule),
   }
 ];
 
