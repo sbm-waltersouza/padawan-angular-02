02b4d9d051739817a48f0811786e7ed5309f49c0
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-06-10 11:40:17 -0300

    Validação Padrão

--- gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuarioService } from './novo-usuario.service';
 
 @Component({
@@ -15,8 +15,12 @@ export class NovoUsuarioComponent implements OnInit {
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
-      email: [''],
-      fullName: [''],
+      email: ['', [
+        Validators.required, Validators.email
+      ]],
+      fullName: ['', [
+        Validators.required, Validators.minLength(4)
+      ]],
       userName: [''],
       password: [''],
     });
@@ -24,7 +28,7 @@ export class NovoUsuarioComponent implements OnInit {
 
   cadastrar() {
     const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
-    Formulário Reativo
+    console.log(novoUsuario);
   }
 
 
