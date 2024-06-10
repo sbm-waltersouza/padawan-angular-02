df015aab832c259d2fdb113f911b318d338cc6c8
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-06-10 13:26:40 -0300

    Validações Assíncronas

--- gatitobook/src/app/home/novo-usuario/minusculo.validator.ts
+++ gatitobook/src/app/home/novo-usuario/minusculo.validator.ts
@@ -1,3 +1,5 @@
+import { AbstractControl } from '@angular/forms';
+
 export function minusculoValidator(control: AbstractControl) {
   const valor = control.value as string;
   if (valor !== valor.toLowerCase()) {
