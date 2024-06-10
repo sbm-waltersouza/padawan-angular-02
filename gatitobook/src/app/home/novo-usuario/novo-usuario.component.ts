77f45481fc8f1e0589d719bab7488edaf3ff6144
sbm-waltersouza <walter.silva@sbmtechnology.com.br>
2024-06-10 10:33:27 -0300

    Formulário Reativo

--- gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NovoUsuarioService } from './novo-usuario.service';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NovoUsuarioComponent implements OnInit {
 
-  constructor() { }
+  novoUsuarioForm!: FormGroup;
+
+  constructor(private formBuilder:FormBuilder, private novoUsuarioService: NovoUsuarioService) { }
 
   ngOnInit(): void {
+    this.novoUsuarioForm = this.formBuilder.group({
+      email: [''],
+      fullName: [''],
+      userName: [''],
+      password: [''],
+    });
   }
+
+  cadastrar() {
+    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+    Formulário Reativo
+  }
+
+
+
 
 }
